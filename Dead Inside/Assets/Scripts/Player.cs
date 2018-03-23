ba7161ba5c142743527816ab1a0f5199c3d19d8e
Marco Aurelio Pavini Mondini <marco-aurelio.pm@hotmail.com>
2018-03-22 22:40:06 -0300

    Flip & Walk
    Adicionado flip And Walk e o animator para trocar animçaõ de idle para walk.

--- Dead Inside/Assets/Scripts/Player.cs
+++ Dead Inside/Assets/Scripts/Player.cs
@@ -2,24 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     private Rigidbody2D rb;
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float horizontal = Input.GetAxis("Horizontal");
+    private Animator myAnimator;
 
-        HandleMovement();
+    [SerializeField]
+    private float movementSpeed;
+
+    private bool facingRight;
+
+    // Use this for initialization
+    void Start()
+    {
+        facingRight = true;
+        rb = GetComponent<Rigidbody2D>(); //cria um componente rigidbody
+        myAnimator = GetComponent<Animator>(); // cria um componente animator
     }
 
-    private void HandleMovement() {
-        rb.velocity = Vector2.left; // vetor com x = -1 e y= 0, movimento para a esquerda
+    // Update is called once per frame
+    void FixedUpdate()// tem uma atualização mais constante do que o Update comum
+    {
+        float horizontal = Input.GetAxis("Horizontal");// atribui o eixo X a variavel horizontal
 
+        //chamada das funções a serem executadas a cada update
+        HandleMovement(horizontal);
+
+        Flip(horizontal); 
     }
+
+    
+    private void HandleMovement(float horizontal) //reponsavel por movimentar o personagem
+    {
+        rb.velocity = new Vector2(horizontal * movementSpeed, rb.velocity.y);
+
+        //troca a animação de Idle para Walk com base na velocidade
+        myAnimator.SetFloat("speed", Mathf.Abs (horizontal));
+    }
+
+    //Muda o personagem de posição de acordo com a direção que ele está indo
+    private void Flip(float horizontal)
+    {
+        if(horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+
+            //muda o personagem de lado com base em sua escala
+            //1 para direita -1 para esquerda
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+
+        }
+    }
+
 }
