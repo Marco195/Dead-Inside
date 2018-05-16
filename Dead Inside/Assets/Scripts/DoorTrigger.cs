5785f3c62c2becf6604489248b8ce712b31dc10a
Marco Aurelio <marco-aurelio.pm@hotmail.com>
2018-05-15 22:50:29 -0300

    Level Won Complete
    Animação de fim de jogo e transição entre fases implementada.

---
+++ Dead Inside/Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour {
+
+    public GameMaster gameMaster;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //compara a tag, se for Player, chama CompleteLevel do GM
+        if(collision.isTrigger != true && collision.CompareTag("Player"))
+        {
+            gameMaster.CompleteLevel();
+        }      
+
+    }
+}
--- Dead Inside/Assets/Scripts/GameMaster.cs
+++ Dead Inside/Assets/Scripts/GameMaster.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour {
     //GameMaster fará a ligação entre das fases (resumindo)
-
+    //IMPORTANTE: Deve haver apenas um GM para todas as fases existentes
     public static GameMaster gm;
 
     public Transform playerPrefab;
@@ -18,6 +18,9 @@ public class GameMaster : MonoBehaviour {
 
     [SerializeField]
     private GameObject gameOverUI;
+
+    [SerializeField]
+    private GameObject levelWonUI;
 
     private static int _remainingLives = 3;
 
@@ -37,26 +40,65 @@ public class GameMaster : MonoBehaviour {
         get { return pontuation; }
     }
 
-    void Start()
+    private void Awake()
     {
         if (gm == null)
         {
             gm = this;
         }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+
         //para iniciar com 3 vidas após o game over
         _remainingLives = maxLives;
-        
+
         //inicia a pontuação
         pontuation = startingpontuation;
-        pontuation = 0; 
+        pontuation = 0;
     }
 
+    //#region Start
+    //void Start()
+    //{
+    //    if (gm == null)
+    //    {
+    //        gm = this;
+    //    }
+    //    //para iniciar com 3 vidas após o game over
+    //    _remainingLives = maxLives;
+        
+    //    //inicia a pontuação
+    //    pontuation = startingpontuation;
+    //    pontuation = 0; 
+    //}
+    //#endregion
+
+    #region CompleteLevel
+    public void CompleteLevel()
+    {
+        Debug.Log("Level WON");
+        extraLive();
+        levelWonUI.SetActive(true); 
+    }
+    #endregion
+
+    public static void End()
+    {
+        gm.EndGame();
+    }
+
+
+    #region EndGame
     public void EndGame()
     {
        Debug.Log("GAME OVER");
        gameOverUI.SetActive(true);
     }
+    #endregion
 
+    #region RespawnPlayer
     public IEnumerator RespawnPlayer()
     {
         //Você usa uma instrução yield return para retornar cada elemento individualmente.        
@@ -65,7 +107,9 @@ public class GameMaster : MonoBehaviour {
         //possivel adicionar um som de respawn aqui
         Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
     }
+    #endregion
 
+    #region KillPlayer
     public static void KillPlayer(Player player)// elimina o jogador
     {
         Destroy(player.gameObject);        
@@ -80,12 +124,15 @@ public class GameMaster : MonoBehaviour {
         }
         
     }
+    #endregion
 
+    #region KillEnemy
     public static void KillEnemy(Enemy enemy) // elimina o inimigo
     {
         gm._KillEnemy(enemy);
     }
 
+    
     public void _KillEnemy(Enemy _enemy)
     {
         pontuation += _enemy.points;//variável points do enemy script
@@ -93,12 +140,16 @@ public class GameMaster : MonoBehaviour {
         Destroy(_enemy.gameObject);
         Score();
     }
+    #endregion
 
+    #region Score
     static void Score()
     {
         PlayerPrefs.SetInt("Score", pontuation);
     }
+    #endregion
 
+    #region extraLive
     //adiciona + 1 quando o jogador fizer 5 pontos
     private void extraLive()
     {
@@ -107,4 +158,5 @@ public class GameMaster : MonoBehaviour {
             _remainingLives += 1; 
         }
     }
+    #endregion
 }
